d4f8c15b358126aa0d9ab9e9affd417302338b67
George Paley <George@JDPaley.com>
2017-06-02 07:42:05 -0400

    First attempt after VirtualBox collapse.

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+
+public class Person {
+			
+	private String[] name  = new String[10];
+	private String[] email = new String[10];
+	
+	private String[] eduAchievement	= new String[10]; 
+	private String[] workExperience 	= new String[10];
+	private String[] skill 			= new String[20];
+	private String[] skillLevel 		= new String[20];
+	public String[] getName() {
+		return name;
+	}
+	public void setName(String[] name) {
+		this.name = name;
+	}
+	public String[] getEmail() {
+		return email;
+	}
+	public void setEmail(String[] email) {
+		this.email = email;
+	}
+	public String[] getEduAchievement() {
+		return eduAchievement;
+	}
+	public void setEduAchievement(String[] eduAchievement) {
+		this.eduAchievement = eduAchievement;
+	}
+	public String[] getWorkExperience() {
+		return workExperience;
+	}
+	public void setWorkExperience(String[] workExperience) {
+		this.workExperience = workExperience;
+	}
+	public String[] getSkill() {
+		return skill;
+	}
+	public void setSkill(String[] skill) {
+		this.skill = skill;
+	}
+	public String[] getSkillLevel() {
+		return skillLevel;
+	}
+	public void setSkillLevel(String[] skillLevel) {
+		this.skillLevel = skillLevel;
+	}
+	
+
+	
+	
+	
+
+}
---
+++ src/RoboResume.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RoboResume {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		print("RESUME ENTRY FORM");
+		print(" ");
+		
+		
+		
+		public static print(String msg) {
+			System.out.println(msg);
+		}
+	}
+
+}
