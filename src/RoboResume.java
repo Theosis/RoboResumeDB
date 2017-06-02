6f6de274d3b792c7d50927735e1ccf160e411a0f
George Paley <George@JDPaley.com>
2017-06-02 07:55:07 -0400

    Even print() undefinable - back to freaking System.out....

--- src/RoboResume.java
+++ src/RoboResume.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
 public class RoboResume {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		print("RESUME ENTRY FORM");
-		print(" ");
+		System.out.println("RESUME ENTRY FORM");
+		System.out.println(" ");
 		
 		
 		
-		public static print(String msg) {
-			System.out.println(msg);
-		}
 	}
 
 }
